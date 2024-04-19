59972724110e81170cf91f82568617f62df6c6d5
aidannelsonuno <aidannelson@unomaha.edu>
2024-04-19 13:23:50 -0500

    Bug fix and type hinting

--- television.py
+++ television.py
@@ -4,42 +4,39 @@ class Television:
     MIN_CHANNEL = 0
     MAX_CHANNEL = 3
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.__status = False
         self.__muted = False
         self.__volume = self.MIN_VOLUME
         self.__channel = self.MIN_CHANNEL
 
-    def power(self):
+    def power(self) -> None:
         self.__status = False if self.__status else True
         
-    def mute(self):
+    def mute(self) -> None:
         if self.__status:
             self.__muted = False if self.__muted else True
 
-    def channel_up(self):
+    def channel_up(self) -> None:
         if self.__status:
-            self.__channel += 1 if self.__channel < self.MAX_CHANNEL else self.MIN_CHANNEL
+            self.__channel = self.__channel + 1 if self.__channel < self.MAX_CHANNEL else self.MIN_CHANNEL
 
-    def channel_down(self):
+    def channel_down(self) -> None:
         if self.__status:
-            self.__channel -= 1 if self.__channel > self.MIN_CHANNEL else self.MAX_CHANNEL
+            self.__channel = self.__channel - 1 if self.__channel > self.MIN_CHANNEL else self.MAX_CHANNEL
 
-    def volume_up(self):
+    def volume_up(self) -> None:
         if self.__status:
             self.__muted = False
             if self.__volume < self.MAX_VOLUME:
                 self.__volume += 1
 
-    def volume_down(self):
+    def volume_down(self) -> None:
         if self.__status:
             self.__muted = False
             if self.__volume > self.MIN_VOLUME:
                 self.__volume -= 1
 
-    def __str__(self):
+    def __str__(self) -> str:
         true_volume = 0 if self.__muted else self.__volume
         return f"Power = {self.__status}, Channel = {self.__channel}, Volume = {true_volume}"
-
-
-    #test
